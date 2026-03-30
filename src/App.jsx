d0465a719e12de7f3244317e1eb60284486e2da4
Mirza Ahmed Zubayer <azubayer632@gamil.com>
2026-03-30 21:40:16 +0600

    made navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import Banner from './components/banner/Banner'
 import Navbar from './components/header/Navbar'
 function App() {
 
   return (
     <>
+      {/* header section */}
       <header>
         <Navbar></Navbar> 
       </header>
+      {/* main section */}
+      <main>
+        {/* banner section */}
+        <Banner></Banner>
+      </main>
+
+
   
     </>
   )
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center mx-48 mt-2 p-2 '>
-            <div className='gradient-text font-bold text-4xl'>DigiTools</div>
-            <div >
-                <ul className='flex justify-around items-center  gap-5 p-4'>
+        <div className='flex justify-between items-center mx-auto lg:mx-48 mt-2 p-2 '>
+            <div className='gradient-text font-bold text-2xl md:text-4xl'>DigiTools</div>
+            <div className='hidden md:block'>
+                <ul className='flex justify-around items-center  md:gap-5 p-4'>
                     <li>Products</li>
                     <li>Features</li>
                     <li>Pricing</li>
@@ -17,7 +17,7 @@ const Navbar = () => {
             <div className='flex items-center gap-4 '>
                 <ShoppingCart />
                 <span className='font-semibold'>LogIn</span>
-                <button className='px-5 py-3 font-semibold text-white gradient-bg rounded-3xl'>Get Started</button>
+                <button className=' font-semibold text-white gradient-bg px-5 md: py-3 rounded-3xl '>Get Started</button>
             </div>
 
         </div>
