f488dba00fe5471a6ed9fa297fca3b836f2bde70
Mirza Ahmed Zubayer <azubayer632@gamil.com>
2026-03-30 20:17:25 +0600

    created initial setup and added necessary dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-5xl text-center text-red-700 bg-cyan-200'>Hello from React Js</h1>
+  
+    </>
+  )
+}
+
+export default App
